52a4b837cc512eaaad54ee055721cdd0fc1b0b41
kaushalrola <kaushalrola@gmail.com>
2022-02-04 10:17:51 +0530

    kp

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb+srv://kaushal:rola@cluster0.o0ojd.mongodb.net/mydb?retryWrites=true&w=majority', { useNewUrlParser: true}).then(() => {
-    console.log("db connected")
-});
+mongoose.connect('mongodb://localhost/mydb', { useNewUrlParser: true});
 var db = mongoose.connection;
 
 // Added check for DB connection
@@ -27,14 +25,14 @@ else
     console.log("Db connected successfully")
 
 // Setup server port
-var port = process.env.PORT||5000;
+var port = process.env.PORT || 8080;
 
 // Send message for default URL
-app.get('/', (req, res) => res.sendfile('index.html',{root:__dirname}));
+app.get('/', (req, res) => res.send('Hello World with Express'));
 
 // Use Api routes in the App
 app.use('/api', apiRoutes);
 // Launch app to listen to specified port
 app.listen(port, function () {
-    console.log("Running KpHub on port " + port);
+    console.log("Running RestHub on port " + port);
 });
