c5882cbfc23ce8840415d0c8255ddbd0eb3eee48
kaushalrola <kaushalrola@gmail.com>
2022-02-04 08:51:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,5 +34,5 @@ app.get('/', (req, res) => res.send('Hello World with Express'));
 app.use('/api', apiRoutes);
 // Launch app to listen to specified port
 app.listen(port, function () {
-    console.log("Running RestHub on port " + port);
+    console.log("Running KpHub on port " + port);
 });
